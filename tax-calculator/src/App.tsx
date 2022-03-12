17bfcad0a5ade96bd7784a3bc915dfd4740d0640
kingchan818 <61489623+kingchan818@users.noreply.github.com>
2022-03-12 19:02:06 +0800

    usestate

--- tax-calculator/src/App.tsx
+++ tax-calculator/src/App.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-
+import { status } from './class/Person';
+import { Tax } from './class/Tax';
 function App() {
+    const [] = useState();
     return <div className="App"></div>;
 }
 
